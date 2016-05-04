8635eeda2cdf387d715ef0fdebf949dec46eb451
Luke Poppe <luke.poppe@kineticdata.com>
2016-05-04 11:54:49 -0500

    kinetic_request_ce_users_retrieve_v1

---
+++ handlers/kinetic_request_ce_users_retrieve_v1/test/simple_input.rb
@@ -0,0 +1,12 @@
+{
+  'info' => {
+    'api_server' => 'https://localhost:8080/kinetic',
+    'api_username' => 'usere@kineticdata.com',
+    'api_password' => 'password',
+    'space_slug' => 'acme'
+
+  },
+  'parameters' => {
+    'space_slug' => ''
+  }
+}
