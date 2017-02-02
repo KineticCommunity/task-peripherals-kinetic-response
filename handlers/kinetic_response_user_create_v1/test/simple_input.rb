31f68b4e968b7b5456d68ed24743f7d9dd6a85ba
James Davies <jameswd89@gmail.com>
2017-02-01 22:07:53 -0600

    Updated User Create Handler to check if User exists first

---
+++ handlers/kinetic_response_user_create_v1/test/simple_input.rb
@@ -0,0 +1,15 @@
+{
+  'info' => {
+    'api_server' => '',
+    'api_username' => '',
+    'api_password' => '',
+    'enable_debug_logging' => 'true'
+  },
+  'parameters' => {
+    'error_handling' => 'Error Message',
+    'name' => 'Joe Shmoe',
+    'email' => 'joe@gmail.com',
+    'password' => '89**aabbFF()ss',
+    'type' => 'Host'
+  }
+}
